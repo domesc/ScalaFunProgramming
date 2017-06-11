794a15f2b05291417f477cd6f25074a39f0d3fe2
domesc <domenico.scarmozzino@gmail.com>
2017-06-11 19:16:19 +0200

    some more parallel functions

--- src/main/scala/parallelism/Par.scala
+++ src/main/scala/parallelism/Par.scala
@@ -2,9 +2,6 @@ package parallelism
 
 import java.util.concurrent._
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-
 /**
   * Created by domesc on 07/06/17.
   */
@@ -14,16 +11,60 @@ object Par {
   def run[A](op: Par[A])(es: ExecutorService): Future[A] = op(es)
 
   /** Take an unevaluated computation and return a [[Par]] object which can run in a separate thread */
-  def unit[A](a: => A): Par[A] = (es: ExecutorService) => Future(a)
+  def unit[A](a: => A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
+  case class UnitFuture[A](value: A) extends Future[A] {
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+
+    override def isCancelled: Boolean = false
+
+    override def isDone: Boolean = true
+
+    override def get(): A = value
+
+    override def get(timeout: Long, unit: TimeUnit): A = get
+  }
+
+  /** Fork the computation in a separate thread */
+  def fork[A](a: => Par[A]): Par[A] = es => es.submit(new Callable[A] {
+    def call = a(es).get
+  })
+
+  /**
+    * Combines the result of two parallel operations
+    */
   def map2[A, B, C](op1: Par[A], op2: Par[B])(combine: (A, B) => C): Par[C] = {
     (es: ExecutorService) => {
 
-      for {
-        future1 <- op1(es)
-        future2 <- op2(es)
-      } yield combine(future1, future2)
+      val future1 = op1(es)
+      val future2 = op2(es)
+
+      UnitFuture(combine(future1.get, future2.get))
     }
+  }
+
+  /** Wraps its unevaluated argument in a [[Par]] and marks it for concurrent evaluation */
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  /**
+    * Convert any function to one that evaluates its result asynchronously
+    */
+  def asyncF[A, B](f: A => B): A => Par[B] = (a: A) => lazyUnit(f(a))
+
+  /** Collect asynchronous results */
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = {
+    ps.foldLeft(unit(List.empty[A])){case (acc, x) => map2(acc, x)(_ :+ _)}
+  }
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  /** Filter elements of a [[List]] in parallel */
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+    val parList = parMap(as)(a => if(f(a)) List(a) else List.empty[A])
+    map2(parList, unit(()))((a, _) => a.flatten)
   }
 
   def sum(ints: IndexedSeq[Int]): Par[Int] = {
--- src/test/scala/parallelism/ParTests.scala
+++ src/test/scala/parallelism/ParTests.scala
@@ -2,25 +2,30 @@ package parallelism
 
 import java.util.concurrent.{ExecutorService, Executors}
 
-import org.scalatest.{AsyncFlatSpec, Matchers}
-
-import scala.concurrent.Future
+import org.scalatest.{FlatSpec, Matchers}
+import parallelism.Par.UnitFuture
 
 /**
   * Created by domesc on 07/06/17.
   */
-class ParTests extends AsyncFlatSpec with Matchers {
+class ParTests extends FlatSpec with Matchers {
 
   behavior of "Par"
 
   val pool: ExecutorService = Executors.newFixedThreadPool(2)
 
   it should "combine two Par instances" in {
-    val op1 = (es: ExecutorService) => Future(2000)
-    val op2 = (es: ExecutorService) => Future(3000)
+    val op1 = (es: ExecutorService) => UnitFuture(2000)
+    val op2 = (es: ExecutorService) => UnitFuture(3000)
 
     val result = Par.map2(op1, op2)(_ + _)
-    Par.run(result)(pool) map (res => res shouldEqual 5000)
+    Par.run(result)(pool).get shouldEqual 5000
+  }
+
+  it should "filter in parallel from a List" in {
+    val list = List(0, 1, 2, 3, 4, 5, 6)
+    val result = Par.parFilter(list)(_ % 2 == 0)
+    Par.run(result)(pool).get shouldEqual List(0, 2, 4, 6)
   }
 
 }
